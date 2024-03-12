8e1d9c14c53d6e159613f0173a9d284ceb87b323
Ne0sky <99585944+Ne0sky@users.noreply.github.com>
2024-03-12 19:27:22 +0530

    update Home page styling

--- src/Components/Service_card.jsx
+++ src/Components/Service_card.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Service_card = ({icon, heading, desc}) => {
+const Service_card = ({src, heading, desc}) => {
   return (
-    <div className='p-4 w-72 bg-white h-48 text-center hover:shadow-xl shadow-lg font-main flex flex-col justify-center items-center rounded-lg'>
-      <p>{icon}</p>
+    <div className='p-4 w-72 bg-white text-center hover:shadow-xl shadow-lg font-main flex flex-col justify-center items-center rounded-lg'>
+      <img src={src} alt="" />
       <h3 className='text-2xl font-semibold py-4'>{heading}</h3>
-      <p className='text-sm'>{desc}</p>
+      <p className='text- font-medium'>{desc}</p>
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import Marquee from "react-fast-marquee";
 import useGetJobs from '../hooks/useGetJobs';
 import JobCard from '../Components/JobCard';
 import CircularProgress from '@mui/material/CircularProgress';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import teamData from '../data/Team';
 import { MdArrowOutward } from "react-icons/md";
 import TeamCard from '../Components/TeamCard';
@@ -16,29 +16,27 @@ const Home = () => {
     window.open(`/job/${_id}`, '_blank');
   };
   return (
-    <div className='font-main  flex flex-col px-2 md:px-4 lg:px-8  w-full  items-center overflow-x-hidden  justify-around '>
-      <div className='flex bg-gray-50 min-h-screen flex-col md:flex-row-reverse px-4 md:px-8 lg:px-32'>
+    <div className='font-main  flex flex-col px-4   w-full  items-center overflow-x-hidden  justify-center '>
+      <div className='flex hero w-screen min-h-screen justify-center flex-col md:flex-row-reverse px-4 md:px-8 lg:px-32'>
       <div className='lg:w-1/2 w-full md:flex justify-center hidden  items-center '>
       <img className='w-96 md:w-80 lg:w-[450px] rounded-xl' src="/hero.png" alt="" />
       </div>
       <div className='lg:w-1/2 py-8 w-full flex justify-center items-start flex-col'>
-        <h1 className='text-2xl lg:text-4xl  font-bold mb-4 text-black'>Elevate Your Business with  <span className=''>Tailor-Made Tech Brilliance!</span></h1>
-        <p className='lg:text-lg font-medium '>Scaling Engineering Teams and Delivering Unparalleled Development Services in AI, ML, Data Science, Web and Mobile Applications</p>
-        <button className='bg-zinc-900 my-8 px-4 py-2 rounded font-semibold text-xl text-white'>Join us now</button>
+        <h1 className='text-2xl lg:text-4xl border-l-4 border-blue-800 px-4 font-bold mb-4 text-black'>Elevate Your Business with  <span className='font-extrabold text-blue-800'>Tailor-Made Tech Brilliance!</span></h1>
+        <p className='lg:text-xl font-medium py-8'>Scaling Engineering Teams and Delivering Unparalleled Development Services in AI, ML, Data Science, Web and Mobile Applications</p>
+        <NavLink to='/careers' className='bg-blue-800 hover:bg-blue-900 shadow-xl shadow-blue-300  px-4 py-4  rounded font-semibold text-xl text-white'>Join us now</NavLink>
         {/* <div  class="bg-gradient-to-r  my-4 from-zinc-100 to-transparent p-4 border-l-4 border-blue-500 ">
           <span className='text-xl font-bold '>For any enquiries please  </span>
           <div className='text-xl font-bold text-blue-500'>WhatsApp at <span className='whitespace-nowrap'>+91 77807 34983</span></div>
         </div> */}
         
-        <Marquee className='bg-zinc-200 py-2 font-bold text-xl' pauseOnHover >
-          <p className='px-2'>Hiring going on for full time positions and internships. Apply now! </p>
-        </Marquee>
+        
       </div>
    
       </div>
       
       
-      {/* Job Openings Section*/}
+      {/* Job Openings Section
       <div className='w-full md:w-[80%] lg:w-[70%]'>
           {
             isLoading ? (
@@ -61,8 +59,34 @@ const Home = () => {
             ) : null
 
           }
-      </div>
+      </div> */}
       <Service_section />
+      {/* More than developers */}
+      <h3 className='text-3xl font-bold'>More than just Developers !</h3>
+
+      <div className='flex flex-col py-16 lg:flex-row justify-center items-center'>
+        <div className='w-full lg:w-1/2 grid grid_cols-1 gap-4 md:grid-cols-2 justify-center items-center '>
+          <div className=''>
+            <h1 className='text-xl font-bold'>Engineering Mindset</h1>
+            <p className='py-8'>Every developer at Primathon works with an Engineering mindset. This allows us to work on creative solutions and build our own libraries uniquely.</p>
+          </div>
+          <div className=''>
+            <h1 className='text-xl font-bold'>Engineering Mindset</h1>
+            <p className='py-8'>From start to end, we follow agile methodology on our projects. We execute the process for continuous improvement discovering solutions for you.</p>
+          </div>
+          <div className=''>
+            <h1 className='text-xl font-bold'>Engineering Mindset</h1>
+            <p className='py-8'>You don’t have to think about dealing with any delays. Our expert team is confident to design, develop, and deliver on time for a seamless experience.</p>
+          </div>
+          <div className=''>
+            <h1 className='text-xl font-bold'>Engineering Mindset</h1>
+            <p className='py-8'>How end users interact with the product becomes extremely important in this journey. We understand and value this in our software building process.</p>
+          </div>
+        </div>
+        <div className='w flex justify-center items-center'>
+          <img className='w-96' src="/more_than_dev.svg" alt="" />
+        </div>
+      </div>
       {/* Team Section */}
       <div className='py-12 flex flex-col items-center justify-center'>
         <h3 className='text-3xl font-bold'>Meet Our Team</h3>
